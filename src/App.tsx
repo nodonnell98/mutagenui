666553a9519a3b9b9c918391d7f1fa7f25d9bae0
Niall O'Donnell <niall_od98@hotmail.com>
2023-11-21 22:37:45 +0000

    basic app page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,73 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+interface Character {
+  name: string;
+  description: string;
+  race_id: number;
+  current_health: number;
+  level: number;
+  skills: {
+    [key: string]: string;
+  };
+  traits: {
+    [key: string]: string;
+  };
+  race: {
+    [key: string]: string;
+  };
+}
+
+function App(): JSX.Element {
+  const [character, setCharacter] = React.useState<Character | null>(null);
+  useEffect(() => { 
+    fetch('http://localhost:3000/characters/1')
+      .then(response => response.json())
+      .then((data: Character) => setCharacter(data))
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="">
+      <header className="">
+        <h1>Mutagen</h1>
       </header>
+      <div>
+        <h2>{character?.name}</h2>
+        <p>{character?.description}</p>
+        <p>Health: {character?.current_health}</p>
+        <p>Level: {character?.level}</p>
+        
+        <div>
+          <h3>Race:</h3>
+          <ul>
+            {character?.race && Object.entries(character.race).map(([race, value]) => (
+              <li key={race}>
+                {race}: {value}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <h3>Traits:</h3>
+          <ul>
+            {character?.traits && Object.entries(character.traits).map(([trait, value]) => (
+              <li key={trait}>
+                {trait}: {value}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <h3>Skills:</h3>
+          <ul>
+            {character?.skills && Object.entries(character.skills).map(([skill, proficiency]) => (
+              <li key={skill}>
+                {skill}: {proficiency}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
